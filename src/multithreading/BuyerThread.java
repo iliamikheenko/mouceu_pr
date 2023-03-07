e13b9029f18c9959e27a09537b5bcc017100b36a
mouceu237 <ilyamikheenko@gmail.com>
2023-03-07 18:06:55 +0400

    added multithreading package

---
+++ src/multithreading/BuyerThread.java
@@ -0,0 +1,35 @@
+package multithreading;
+
+import java.util.Queue;
+
+public class BuyerThread implements Runnable{
+    private final Queue<Cashbox> cashboxes;
+
+    public BuyerThread(Queue<Cashbox> cashboxes) {
+        this.cashboxes = cashboxes;
+    }
+
+    @Override
+    public void run() {
+        try {
+            synchronized (cashboxes){
+                while (true){
+                    if (!cashboxes.isEmpty()) {
+                        var cashbox = cashboxes.remove();
+                        System.out.println(Thread.currentThread().getName() + " обслуживается в кассе " + cashbox);
+                        cashboxes.wait(5L);
+                        System.out.println(Thread.currentThread().getName() + " освобождаю кассу " + cashbox);
+                        cashboxes.add(cashbox);
+                        cashboxes.notifyAll();
+                        break;
+                    } else {
+                        cashboxes.wait();
+                        System.out.println(Thread.currentThread().getName() + " ожидает свободную кассу");
+                    }
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/multithreading/CahboxRunner.java
@@ -0,0 +1,34 @@
+package multithreading;
+
+import java.util.ArrayDeque;
+import java.util.List;
+import java.util.Queue;
+import java.util.stream.Stream;
+
+public class CahboxRunner {
+    public static void main(String[] args) {
+        Queue<Cashbox> cashboxes = new ArrayDeque<>(List.of(new Cashbox(), new Cashbox()));
+        List<Thread> threads = Stream.of(new BuyerThread(cashboxes),
+                        new BuyerThread(cashboxes),
+                        new BuyerThread(cashboxes),
+                        new BuyerThread(cashboxes),
+                        new BuyerThread(cashboxes),
+                        new BuyerThread(cashboxes),
+                        new BuyerThread(cashboxes),
+                        new BuyerThread(cashboxes),
+                        new BuyerThread(cashboxes),
+                        new BuyerThread(cashboxes),
+                        new BuyerThread(cashboxes),
+                        new BuyerThread(cashboxes),
+                        new BuyerThread(cashboxes))
+                .map(Thread::new)
+                .peek(Thread::start)
+                .toList();
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();            }
+        }
+    }
+}
